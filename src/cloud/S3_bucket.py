ac85d3cad9095ff524f6bb819e058fa11577115f
goyumsamuditha <goyumsamuditha@gmail.com>
2026-03-24 06:27:55 +0530

    Update requirement file

---
+++ src/cloud/S3_bucket.py
@@ -0,0 +1,60 @@
+import os
+import boto3
+from dotenv import load_dotenv
+import json
+
+# load environment variables
+load_dotenv()
+s3 = boto3.client(
+    aws_access_Key_id = os.getenv('AWS_Access_KEY') ,       # access key
+    aws_secret_access_key = os.getenv('AWS_SECRET_KEY'),    # secret key
+    region_name           = os.getenv('AWS_REGION', 'eu-north-1'), # region
+
+)
+
+bucket = os.getenv('s3_bucket')  # 
+
+def upload_file_to_s3(file_path, s3_key):   
+    """upload data files to S3 bucket """
+    s3.upload_file(file_path, bucket, s3_key)   # upload file to S3
+    print(f"File {file_path} uploaded to S3 bucket {bucket} with key {s3_key}") # print confirmation
+
+
+def download_file_from_s3(s3_key, local_path):
+    """Download file from S3"""
+    os.makedirs(os.path.dirname(local_path), exist_ok=True)  # ensure local directory exists
+    s3.download_file(bucket, s3_key, local_path)   # download file from S3
+    print(f"File with key {s3_key} downloaded from S3 bucket {bucket} to {local_path}") # print confirmation
+
+def upload_raw_data():
+    """Upload all raw data files from the local directory to the S3 bucket."""
+    local_dir = 'data/raw'
+    for filename in os.listdir(local_dir):
+        if filename.endswith('.csv'):
+            upload_file_to_s3(f'data/raw(f)',f'raw/{filename}')  # upload each file to S3 under the 'raw/' prefix
+
+
+def upload_featured_data():
+    """Upload all featured data files from the local directory to the S3 bucket"""
+    local_dir = 'data/featured'
+    for filename in os.listdir(local_dir):
+        if filename.endswith('.csv'):
+            upload_file_to_s3(f'data/featured(f)',f'featured/{filename}')  # upload each file to S3 under the 'featured/' prefix
+
+def upload_models():
+    for filename in ('decision_engine.pkl', 'scaler.pkl'):
+        if os.path.exists(f'models/(f)'):
+            upload_file_to_s3(f'models/(f)',f'models/(f)')         # upload each file to S3 under the 'models/' prefix
+
+def upload_plots():
+    local_dir = 'data/plots'
+     for filename in os.listdir(local_dir):
+        if filename.endswith('.png'):
+            upload_file_to_s3(f'data/plots(f)',f'plots/{filename}')  # upload each file to S3 under the 'plots/' prefix
+
+
+def download_data_EC2():
+    """Download all files from S3 to EC2"""
+    objects = s3.list_v2(Bucket=bucket).get('contents',[])  # 
+
+
