35e8ec9e9a3c3d08a919f81cb55e9c868578b5b9
goyumsamuditha <goyumsamuditha@gmail.com>
2026-04-02 22:43:29 +0530

    Refactor S3_bucket.py to improve file path handling and ensure correct uploads to S3

--- src/cloud/S3_bucket.py
+++ src/cloud/S3_bucket.py
@@ -3,17 +3,20 @@ import boto3 # AWS SDK for Python
 from dotenv import load_dotenv
 import json
 
+from pathlib import Path
+
 # load environment variables
 load_dotenv()
 s3 = boto3.client(
     's3',
-    aws_access_key_id = os.getenv('AWS_Access_KEY') ,       # access key
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY') ,       # access key
     aws_secret_access_key = os.getenv('AWS_SECRET_KEY'),    # secret key
     region_name           = os.getenv('AWS_REGION', 'eu-north-1'), # region
 
 )
 
 bucket = os.getenv('S3_BUCKET')  
+base = Path(__file__).resolve().parent.parent.parent  # base directory of the project
 
 def upload_file_to_s3(file_path, s3_key):   
     """upload data files to S3 bucket """
@@ -29,36 +32,38 @@ def download_file_from_s3(s3_key, local_path):
 
 def upload_raw_data():
     """Upload all raw data files from the local directory to the S3 bucket."""
-    local_dir = 'data/raw'
+    local_dir = base / 'data' / 'raw'
     for filename in os.listdir(local_dir):
         if filename.endswith('.csv'):
-            upload_file_to_s3(f'data/raw/{filename}',f'raw/{filename}')  # upload each file to S3 under the 'raw/' prefix
+            upload_file_to_s3(str(local_dir / filename), f'raw/{filename}')  # upload each file to S3 under the 'raw/' prefix
 
 
 def upload_featured_data():
     """Upload all featured data files from the local directory to the S3 bucket"""
-    local_dir = 'data/featured'
+    local_dir = base / 'data' / 'featured'
     for filename in os.listdir(local_dir):
         if filename.endswith('.csv'):
-            upload_file_to_s3(f'data/featured(f)',f'featured/{filename}')  # upload each file to S3 under the 'featured/' prefix
+            upload_file_to_s3(str(local_dir / filename), f'featured/{filename}')  # upload each file to S3 under the 'featured/' prefix
 
 def upload_models():
     """ Upload all model files from the local directory to the S3 bucket"""
-    for filename in ('decision_engine.pkl', 'scaler.pkl'):        
-        if os.path.exists(f'models/(f)'):
-            upload_file_to_s3(f'models/(f)',f'models/(f)')  # upload each file to S3 under the 'models/' prefix
+    local_dir = base / 'models'
+    for filename in ['decision_engine.pkl', 'scaler.pkl']:
+        filepath = local_dir / filename # construct full file path  
+        if filepath.exists():  # check if file exists
+            upload_file_to_s3(filepath, f'models/{filename}')  # upload each file to S3 under the 'models/' prefix
 
 def upload_plots():
     """ Upload all plot files from the local directory to the S3 bucket"""
-    local_dir = 'data/plots'
+    local_dir = base / 'data' / 'plots'
     for filename in os.listdir(local_dir):
         if filename.endswith('.png'):
-            upload_file_to_s3(f'data/plots(f)',f'plots/{filename}')  # upload each file to S3 under the 'plots/' prefix
+            upload_file_to_s3(str(local_dir / filename), f'plots/{filename}')  # upload each file to S3 under the 'plots/' prefix
 
 
 def download_data_EC2():
     """Download all files from S3 to EC2"""
-    objects = s3.list_v2(Bucket=bucket).get('contents',[])  # list all objects in the S3 bucket
+    objects = s3.list_objects_v2(Bucket=bucket).get('Contents',[])  # list all objects in the S3 bucket
     for obj in objects:
         key = obj['Key']  # get the key of each object
         local = key # set local path same as key
