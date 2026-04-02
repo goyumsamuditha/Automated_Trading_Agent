3cac14ae3f4b6643bf36a5c5e633531f9644368a
goyumsamuditha <goyumsamuditha@gmail.com>
2026-04-02 22:43:05 +0530

    Add upload steps for S3 and RDS to the pipeline

--- src/pipline.py
+++ src/pipline.py
@@ -5,6 +5,8 @@ steps = [
     ('Information Retreval', 'src/info_retrieval.py'),
     ('Model Train', 'src/decision_engine.py'),
     ('Backtest', 'src/backtest.py'),
+    ('upload to S3', 'src/cloud/S3_bucket.py'),
+    ('upload to RDS', 'src/cloud/RDS_Database.py'),
 ]
 
 for name , script in steps:
@@ -15,6 +17,10 @@ for name , script in steps:
     if result.returncode != 0: # Check if the script execution was successful
         print(f'Error occurred during {name} step. Exiting pipeline.') # Print an error message if the script failed
         sys.exit(1) # Exit the pipeline with a non-zero status to indicate failure
+    print(f'{name} step completed successfully.') # Print a success message if the script executed without errors
+    
 
-
+print("\n"+"="*50) # Print a final separator after all steps have been executed
 print('\nPipeline execution completed successfully!') # Print a success message after all steps have been executed
+print('All ressults saved to S3 and RDS.') # Print a message indicating that all results have been saved to S3 and RDS
+print("="*50) # Print a final separator for better readability
